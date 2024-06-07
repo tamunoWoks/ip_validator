eae19c08d93bb35f2a71a332f582cf7621d6508d
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-07 11:47:36 -0600

    Clean up test_valid.py

--- test_valid.py
+++ test_valid.py
@@ -1,23 +1,24 @@
 from valid import validate
 
+
 def test_format():
-    assert validate('1.2.3.4') == True
-    assert validate('1.2.3') == False
-    assert validate('1.2') == False
+    assert validate("1.2.3.4") == True
+    assert validate("1.2.3") == False
+    assert validate("1.2") == False
 
 
 def test_range():
-    assert validate('255.255.255.255') == True
-    assert validate('1000.255.255.255') == False
-    assert validate('255.1000.255.255') == False
-    assert validate('255.255.1000.255') == False
-    assert validate('255.255.255.1000') == False
+    assert validate("255.255.255.255") == True
+    assert validate("1000.255.255.255") == False
+    assert validate("255.1000.255.255") == False
+    assert validate("255.255.1000.255") == False
+    assert validate("255.255.255.1000") == False
 
 
 def test_alpha():
-    assert validate('cat.54.dog.255') == False
-    assert validate('lion') == False
+    assert validate("cat.54.dog.255") == False
+    assert validate("lion") == False
 
 
 def test_firstbyte():
-    assert validate('101.512.511.322') == False
+    assert validate("101.512.511.322") == False
