8a33a539d87580536595b3d25ccf3198999f0191
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-07 11:33:44 -0600

    Update validate(), ensure entire string matches regex pattern

--- valid.py
+++ valid.py
@@ -6,6 +6,11 @@ def main():
 def validate(ip):
     #Regex to match a valid IPv4 address
     pattern = r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+    #Use fullmatch to ensure the entir string matches the pattern
+    if re.fullmatch(pattern, ip):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
