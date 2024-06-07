9751d6b4e6b5a6597905f5ab78f2adc7023cecbb
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-07 11:31:03 -0600

    Create func to check ip address, create regex pattern

--- valid.py
+++ valid.py
@@ -3,6 +3,10 @@ import re
 def main():
     ...
 
+def validate(ip):
+    #Regex to match a valid IPv4 address
+    pattern = r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+
 
 if __name__ == '__main__':
     main()
